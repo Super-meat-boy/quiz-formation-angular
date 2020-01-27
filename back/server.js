87bdc229382b2c1b15303d09e0cf8715cebc48c5
Fab <fabien.terrasse.pf1@gmail.com>
2020-01-27 12:08:36 +0100

    changing port

--- back/server.js
+++ back/server.js
@@ -4,10 +4,13 @@ const app = express();
 
 // quelque soit le verbe http et le '.' indique le répertoire actuel
 app.use(express.static('.'));
+console.log('CARAMBA');
 
 // sers à mettre un système de navigation 
 app.use(serveIndex('.', { icons: true }));
+console.log('CARAMBA');
 
 app.listen(3000, () => {
+    console.log('CARAMBA');
     console.log('Example app listening on port 3000!')
 });
