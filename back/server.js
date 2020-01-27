1465111571ed423b547a1f9290e6b875df3f47e4
Fab <fabien.terrasse.pf1@gmail.com>
2020-01-27 15:40:25 +0100

    remove * as

--- back/server.js
+++ back/server.js
@@ -1,15 +1,12 @@
-const express = require('express');
-const serveIndex = require('serve-index');
-const app = express();
-
+var express = require('express');
+var serveIndex = require('serve-index');
+var app = express();
 // quelque soit le verbe http et le '.' indique le répertoire actuel
-const www = '../front/dist/front'
+var www = '../front/dist/front';
 app.use(express.static(www));
-app.use(serveIndex(www, {icons: true}));
-
+app.use(serveIndex(www, { icons: true }));
 // sers à mettre un système de navigation 
 app.use(serveIndex('.', { icons: true }));
-
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
 });
