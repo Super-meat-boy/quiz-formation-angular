7e82b696e6b0d636d6a21cf39185c446bf4d6b48
Fab <fabien.terrasse.pf1@gmail.com>
2020-01-27 16:23:21 +0100

    change favicon

--- back/server.ts
+++ back/server.ts
@@ -1,15 +1,15 @@
-import express from'express';
-import serveIndex from 'serve-index';
+import express from "express";
+import serveIndex from "serve-index";
 const app = express();
 
 // quelque soit le verbe http et le '.' indique le répertoire actuel
-const www = '../front/dist/front'
+const www = "../front/dist/front";
 app.use(express.static(www));
-app.use(serveIndex(www, {icons: true}));
+app.use(serveIndex(www, { icons: true }));
 
-// sers à mettre un système de navigation 
-app.use(serveIndex('.', { icons: true }));
+// sers à mettre un système de navigation
+app.use(serveIndex(".", { icons: true }));
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
+  console.log("Example app listening on port 3000!");
 });
